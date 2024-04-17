b2f81c5256a1fe1b9a50c7a77cbacb4ca8e89c8b
Harris Ellahi <98173884+hariscoder3@users.noreply.github.com>
2024-04-17 13:49:56 +0500

    Time: 59 ms (23.85%), Space: 12 MB (47.07%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums*2
+    
+#         ans = nums
+        
+#         for i in range(len(nums)):
+#             ans.append(i)
+        
+#         return ans;
+        
