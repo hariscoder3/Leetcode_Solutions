eea8f6c05958463708cda322bce2e235c265f7c9
Harris Ellahi <98173884+hariscoder3@users.noreply.github.com>
2024-06-11 15:38:53 +0500

    Time: 45 ms (64.77%), Space: 16.5 MB (91.55%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,36 +1,12 @@
-class Solution(object):
-    def romanToInt(self, s):
-        integer_value = 0
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hash_map = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         
-        reverse_str = s[::-1]
-        i = 0;
-        
-        for char in reverse_str:
-            if char == "I":
-                integer_value = integer_value+1;
-            elif char == "V"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="I":
-                integer_value = (integer_value+5)-2;
-            elif char == "X"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="I":
-                integer_value = (integer_value+10)-2;
-            elif char == "L"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="X":
-                integer_value = (integer_value+50)-20;
-            elif char == "C"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="X":
-                integer_value = (integer_value+100)-20;
-            elif char == "D"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="C":
-                integer_value = (integer_value+500)-200;
-            elif char == "M"  and (i+1)<len(reverse_str) and reverse_str[i+1]=="C":
-                integer_value = (integer_value+1000)-200;
-            elif char == "V":
-                integer_value = integer_value+5;
-            elif char == "X":
-                integer_value = integer_value+10;
-            elif char == "L":
-                integer_value = integer_value+50;
-            elif char == "C":
-                integer_value = integer_value+100;
-            elif char == "D":
-                integer_value = integer_value+500;
-            elif char == "M":
-                integer_value = integer_value+1000;
-            i=i+1;
-        return integer_value;
+        summ = 0
+        for num in range(len(s)):
+            if num<(len(s)-1) and hash_map[s[num]]<hash_map[s[num+1]]:
+                summ -= hash_map[s[num]]
+            else:
+                summ+=hash_map[s[num]]
+                
+        return summ;
